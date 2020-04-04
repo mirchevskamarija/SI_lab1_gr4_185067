f3f620310685467623d5e0df5be47c9e44a629d4
Marija <mmarija143@gmail.com>
2020-04-04 23:00:50 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -7,16 +7,77 @@ class Point {
 
 	//TODO constructor
 
+	public Point(String id, double x, double y, String boja) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
 	//TODO setters and getters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getBoja() {
+		return boja;
+	}
+
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
+
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if(xDirection == 'L')
+		{
+			x--;
+		}
+		else if(xDirection == 'R')
+		{
+			x++;
+		}
+		if(yDirection == 'U')
+		{
+			y++;
+		}
+		else if(yDirection == 'D')
+		{
+			y--;
+		}
 	}
 
 	public void draw () {
 		//TODO
+		System.out.println("Tockata " + id + " e " + x + " edinici na x " + y + " edinici na y");
 	}
 
+
+
+}
+
+class Canvas{
+	List<Points> points = new List<>;
 
 
 }
